9829cda9607fcd88aa2509f5a9af424eec4913aa
jjulien71 <jjulien71@d118feeb-734d-0410-a58d-71a89b604941>
2008-05-31 21:53:10 +0000

    finished integration of jriihi's multi libraries support

--- src/com/moviejukebox/plugin/MovieDatabasePlugin.java
+++ src/com/moviejukebox/plugin/MovieDatabasePlugin.java
@@ -1,11 +1,15 @@
 package com.moviejukebox.plugin;
 
+import java.util.Properties;
+
 import com.moviejukebox.model.Movie;
 
 public interface MovieDatabasePlugin {
 
-	void scan(Movie movie);
+	public void init(Properties props);
+	
+	public void scan(Movie movie);
 
-	void downloadPoster(String jukeboxRoot, Movie movie);
+	public void downloadPoster(String jukeboxRoot, Movie movie);
 
 }
