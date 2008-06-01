6c778438902c611c450405d4c600bf927a22d712
jjulien71 <jjulien71@d118feeb-734d-0410-a58d-71a89b604941>
2008-06-01 18:53:13 +0000

    refactoring + documentation

--- src/com/moviejukebox/plugin/MovieDatabasePlugin.java
+++ src/com/moviejukebox/plugin/MovieDatabasePlugin.java
@@ -4,12 +4,42 @@ import java.util.Properties;
 
 import com.moviejukebox.model.Movie;
 
+/**
+ * MovieDatabasePlugin classes must implement this interface in order 
+ * to be integrated to moviejukebox.
+ * 
+ * Custom implementations classes must be registered into the 
+ * moviejukebox.properties file (see property "mjb.internet.plugin").
+ * 
+ * Once the class is registered, is called once at the program init,
+ * then for each movie in the library.
+ * 
+ * @author Julien
+ */
 public interface MovieDatabasePlugin {
 
+	/**
+	 * Called by movie jukebox at program initialisation.
+	 * Contains the moviejukebox.properties
+	 * 
+	 * @param props moviejukebox properties
+	 */
 	public void init(Properties props);
-	
+
+	/**
+	 * Called by movie jukebox when processing a movie.
+	 * 
+	 * Scan information for the specified movie.
+	 * The provided movie object conatins at least 
+	 * the following data:
+	 * <ul>
+	 * <li>Title
+	 * <li>Year (can be unknown)
+	 * <li>Season (can be unknown)
+	 * <li>movieFiles (at least one)
+	 * </ul>
+	 * 
+	 * @param movie a <tt>Movie</tt> object to update.
+	 */
 	public void scan(Movie movie);
-
-	public void downloadPoster(String jukeboxRoot, Movie movie);
-
 }
