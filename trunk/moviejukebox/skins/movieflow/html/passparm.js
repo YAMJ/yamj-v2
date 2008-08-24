308c8b159c14e9e95de38e2e9e8c0ac3ad35d7db
JJulien71 <JJulien71@d118feeb-734d-0410-a58d-71a89b604941>
2008-08-24 18:55:57 +0000

    Added new movieflow dedicated skin

---
+++ trunk/moviejukebox/skins/movieflow/html/passparm.js
@@ -0,0 +1,36 @@
+function passparm()
+{    
+   var parameters = location.search.substring(1).split("&");
+   var temp = parameters[0].split("=");
+   var key  = temp[0];              
+
+   if (key=='pnow')
+   {
+     pnow = unescape(temp[1]);
+   }
+
+   temp = parameters[1].split("=");
+   key  = temp[0];
+  
+   if (key=='pselsort')
+   {
+     pselsort = unescape(temp[1]);
+   }
+
+   temp = parameters[2].split("=");
+   key  = temp[0];
+
+   if (key=='pselidx')
+   {
+      pselidx = unescape(temp[1]);
+   }
+  
+   // did we get good parameters
+   if (pnow &&  pselsort &&  pselidx)
+      {
+         parms    = 1;
+         selsort  = parseInt(pselsort);
+         selidx   = parseInt(pselidx);
+         now      = parseInt(pnow);
+      }
+}
