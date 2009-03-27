e1912eeb6cd44f26da325f82da122064f71cf30b
artem.gratchev <artem.gratchev@d118feeb-734d-0410-a58d-71a89b604941>
2009-03-27 21:55:56 +0000

    Issue 642 moviejukebox.properties and skin.properties UTF-8 character set

--- trunk/moviejukebox/src/com/moviejukebox/tools/PropertiesUtil.java
+++ trunk/moviejukebox/src/com/moviejukebox/tools/PropertiesUtil.java
@@ -1,9 +1,12 @@
 package com.moviejukebox.tools;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Properties;
 import java.util.Set;
 import java.util.Map.Entry;
@@ -15,20 +18,23 @@ import java.util.logging.Logger;
  */
 public class PropertiesUtil {
 
-    private static Logger logger = Logger.getLogger("moviejukebox");
+    private static final String PROPERTIES_CHARSET = "UTF-8";
+	private static Logger logger = Logger.getLogger("moviejukebox");
     private static Properties props = null;
 
     public static boolean setPropertiesStreamName(String streamName) {
         logger.fine("Using properties file " + streamName);
         InputStream propertiesStream = ClassLoader.getSystemResourceAsStream(streamName);
-
+        	
         try {
             if (propertiesStream == null) {
                 propertiesStream = new FileInputStream(streamName);
             }
 
+            Reader reader = new InputStreamReader(propertiesStream, PROPERTIES_CHARSET);
             props = new Properties();
-            props.load(propertiesStream);
+            props.load(reader);
+
         } catch (IOException e) {
             logger.severe("Failed loading file " + streamName + ": Please check your configuration. The moviejukebox.properties should be in the classpath.");
             return false;
@@ -42,7 +48,8 @@ public class PropertiesUtil {
         File skinProperties = new File(skinHome, "skin.properties");
         try {
             propertiesStream = new FileInputStream(skinProperties);
-            props.load(propertiesStream);
+            Reader reader = new InputStreamReader(propertiesStream, PROPERTIES_CHARSET);
+            props.load(reader);
         } catch (Exception e) {
             logger.severe("Failed loading file " + skinProperties.getAbsolutePath() + ": Please check your configuration. The moviejukebox.properties should be in the classpath, and define a property called mjb.skin.dir which point to the skin directory.");
             return false;
