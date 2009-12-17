ba5f705e34b850ae80cb3441d0bd0e10040ce149
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2009-12-17 11:50:37 +0000

    Issue 1130 YAMJ gives a titleless episode 00 the same title as episode 01

--- trunk/moviejukebox/src/com/moviejukebox/scanner/MovieFilenameScanner.java
+++ trunk/moviejukebox/src/com/moviejukebox/scanner/MovieFilenameScanner.java
@@ -357,7 +357,6 @@ public class MovieFilenameScanner {
                 while (ematcher.find()) {
                     dto.getEpisodes().add(Integer.parseInt(ematcher.group(1)));
                 }
-
             }
         }
 
