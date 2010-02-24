73ad3a1c93d742d0d93edbb8feecf9f5c904ae7a
Stuart Boston <omertron@gmail.com>
2010-02-24 07:30:12 +0000

    Moved deleteDir function to FileTools

--- src/com/moviejukebox/tools/FileTools.java
+++ src/com/moviejukebox/tools/FileTools.java
@@ -302,4 +302,35 @@ public class FileTools {
 
         return parentFolder;
     }
+
+    /**
+     * Recursively delete a directory
+     * @param dir
+     * @return
+     */
+    public static boolean deleteDir(String dir) {
+        return deleteDir(new File(dir));
+    }
+    
+    /**
+     * Recursively delete a directory 
+     * @param dir
+     * @return
+     */
+    public static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    //System.out.println("Failed");
+                    return false;
+                }
+            }
+        }
+        // The directory is now empty so delete it
+        // System.out.println("Deleting: " + dir.getAbsolutePath());
+        return dir.delete();
+    }
+    
 }
