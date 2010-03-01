bf0b3e4ef81ec0e9c0363715abb0f71dcb29b648
vincent.ysmal <vincent.ysmal@d118feeb-734d-0410-a58d-71a89b604941>
2010-03-01 08:55:39 +0000

    Issue 1311:  	 YAMJ doesn't use MediaInfo's information in vob and mp4 files and some mkv

---
+++ trunk/moviejukebox/test/com/moviejukebox/scanner/MediaInfoScannerTestCase.java
@@ -0,0 +1,29 @@
+package com.moviejukebox.scanner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+
+public class MediaInfoScannerTestCase extends TestCase {
+
+    public void test1() {
+        File file = new File("mediainfo-1.txt");
+        MediaInfoScanner toTest = new MediaInfoScanner();
+        HashMap<String, String> infosGeneral = new HashMap<String, String>();
+        ArrayList<HashMap<String, String>> infosVideo = new ArrayList<HashMap<String, String>>();
+        ArrayList<HashMap<String, String>> infosAudio = new ArrayList<HashMap<String, String>>();
+        ArrayList<HashMap<String, String>> infosText = new ArrayList<HashMap<String, String>>();
+
+        try {
+            toTest.parseMediaInfo(new FileInputStream(file), infosGeneral, infosVideo, infosAudio, infosText);
+            assertEquals(8, infosGeneral.size());
+
+        } catch (Exception e) {
+            assertFalse("No exception expected : " + e.getMessage(), true);
+        }
+
+    }
+}
