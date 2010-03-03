da6c8fa0a849edaa3f920ba0d3a309fd1c30dd38
vincent.ysmal <vincent.ysmal@d118feeb-734d-0410-a58d-71a89b604941>
2010-03-03 22:32:54 +0000

    Extract poster search from PosterScanner to Individual plugins

---
+++ src/com/moviejukebox/plugin/poster/YahooPosterPlugin.java
@@ -0,0 +1,72 @@
+/*
+ *      Copyright (c) 2004-2009 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list 
+ *  
+ *      Web: http://code.google.com/p/moviejukebox/
+ *  
+ *      This software is licensed under a Creative Commons License
+ *      See this page: http://code.google.com/p/moviejukebox/wiki/License
+ *  
+ *      For any reuse or distribution, you must make clear to others the 
+ *      license terms of this work.  
+ */
+
+package com.moviejukebox.plugin.poster;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.logging.Logger;
+
+import com.moviejukebox.model.Movie;
+import com.moviejukebox.tools.WebBrowser;
+
+public class YahooPosterPlugin implements IMoviePosterPlugin {
+    private static Logger logger = Logger.getLogger("moviejukebox");
+    private WebBrowser webBrowser;
+
+    public YahooPosterPlugin() {
+        super();
+        webBrowser = new WebBrowser();
+    }
+
+    @Override
+    public String getIdFromMovieInfo(String title, String year) {
+        // No id from yahoo search, return title
+        return title;
+    }
+
+    @Override
+    public String getPosterUrl(String title, String year) {
+        try {
+            // TODO Change out the French Yahoo for English
+            StringBuffer sb = new StringBuffer("http://fr.images.search.yahoo.com/search/images?p=");
+            sb.append(URLEncoder.encode(title, "UTF-8"));
+            sb.append("+poster&fr=&ei=utf-8&js=1&x=wrt");
+
+            String xml = webBrowser.request(sb.toString());
+            int beginIndex = xml.indexOf("imgurl=");
+            int endIndex = xml.indexOf("%26", beginIndex);
+
+            if (beginIndex != -1 && endIndex > beginIndex) {
+                return URLDecoder.decode(xml.substring(beginIndex + 7, endIndex), "UTF-8");
+            } else {
+                return Movie.UNKNOWN;
+            }
+        } catch (Exception error) {
+            logger.severe("YahooPosterPlugin : Failed retreiving poster URL from yahoo images : " + title);
+            logger.severe("Error : " + error.getMessage());
+            return Movie.UNKNOWN;
+        }
+    }
+
+    @Override
+    public String getPosterUrl(String id) {
+        return getPosterUrl(id, null);
+    }
+
+    @Override
+    public String getName() {
+        return "yahoo";
+    }
+
+}
---
+++ test/com/moviejukebox/plugin/poster/MovieDbPosterPluginTestCase.java
@@ -0,0 +1,31 @@
+/*
+ *      Copyright (c) 2004-2009 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list 
+ *  
+ *      Web: http://code.google.com/p/moviejukebox/
+ *  
+ *      This software is licensed under a Creative Commons License
+ *      See this page: http://code.google.com/p/moviejukebox/wiki/License
+ *  
+ *      For any reuse or distribution, you must make clear to others the 
+ *      license terms of this work.  
+ */
+
+package com.moviejukebox.plugin.poster;
+
+import junit.framework.TestCase;
+
+import com.moviejukebox.tools.PropertiesUtil;
+
+public class MovieDbPosterPluginTestCase extends TestCase {
+
+    public void testGetId() {
+        PropertiesUtil.setProperty("API_KEY_TheMovieDB", "5a1a77e2eba8984804586122754f969f");
+        MovieDbPosterPlugin toTest = new MovieDbPosterPlugin();
+        String idFromMovieInfo = toTest.getIdFromMovieInfo("Gladiator", "2000");
+        assertEquals("98", idFromMovieInfo);
+
+        String posterUrl = toTest.getPosterUrl(idFromMovieInfo);
+        assertEquals("http://images.themoviedb.org/posters/1297/Gladiator.jpg", posterUrl);
+    }
+}
