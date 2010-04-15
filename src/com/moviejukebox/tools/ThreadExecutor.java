d9becead9529914952064808e16e2c5bfe425ff2
gabrielcorneanu@gmail.com <gabrielcorneanu@gmail.com@d118feeb-734d-0410-a58d-71a89b604941>
2010-04-15 13:35:40 +0000

    modified basename to use hash of relative path only (avoid problems on moving around) refined thread executor, many conditions checked to detect problems

--- src/com/moviejukebox/tools/ThreadExecutor.java
+++ src/com/moviejukebox/tools/ThreadExecutor.java
@@ -21,22 +21,30 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ThreadExecutor<T> implements ThreadFactory{
 
-    private Collection<Future<T>> values = new ArrayList<Future<T>>();
-    private ExecutorService pool;
-    private int threads_run, threads_io;
+    private Collection<Future<T>> values = new ArrayList<Future<T>>(100);
+    private ThreadPoolExecutor pool = null;
+    private BlockingQueue<Runnable> queue = null;
+    private int threads_run, threads_io, threads_total;
     private boolean ignoreErrors = true;
     private static Logger logger = Logger.getLogger("moviejukebox");
     private Semaphore runningThreads, ioThreads;
@@ -55,7 +63,7 @@ public class ThreadExecutor<T> implements ThreadFactory{
         // Default, can be overridden
         grouplimits.put(".*", new Semaphore(1));
         String limitsProperty = PropertiesUtil.getProperty("mjb.MaxDownloadSlots", ".*=1");
-        logger.finer("WebBrowser: Using download limits: " + limitsProperty);
+        logger.finer("Using download limits: " + limitsProperty);
 
         Pattern semaphorePattern = Pattern.compile(",?\\s*([^=]+)=(\\d+)");
         Matcher semaphoreMatcher = semaphorePattern.matcher(limitsProperty);
@@ -63,10 +71,10 @@ public class ThreadExecutor<T> implements ThreadFactory{
             String group = semaphoreMatcher.group(1);
             try {
                 Pattern.compile(group);
-                logger.finer("WebBrowser: " + group + "=" + semaphoreMatcher.group(2));
+                logger.finer(group + "=" + semaphoreMatcher.group(2));
                 grouplimits.put(group, new Semaphore(Integer.parseInt(semaphoreMatcher.group(2))));
             } catch (Exception error) {
-                logger.finer("WebBrowser: Limit rule \"" + group + "\" is not valid regexp, ignored");
+                logger.finer("Rule \"" + group + "\" is not valid regexp, ignored");
             }
         }
     }
@@ -88,13 +96,16 @@ public class ThreadExecutor<T> implements ThreadFactory{
     public ThreadExecutor(int threads_run, int threads_io) {
         this.threads_run = threads_run;
         this.threads_io = threads_io <= 0 ? threads_run : threads_io;
+        threads_total = this.threads_run + this.threads_io;
         restart();
     }
 
+    /*
+     * Thread descendant class used for our execution scheduling
+     */ 
     static private class ScheduledThread extends Thread {
         private Semaphore s_run, s_io, s_iotarget;
-        private int io_cnt = 0;
-        private String iohost="none";
+        private Stack<String> hosts = new Stack<String>();
         private ScheduledThread(Runnable r, Semaphore s_run, Semaphore s_io) {
             super(r);
             this.s_run = s_run;
@@ -102,58 +113,83 @@ public class ThreadExecutor<T> implements ThreadFactory{
         }
         @Override
         public void run() {
+            s_run.acquireUninterruptibly();
             try{
-                s_run.acquireUninterruptibly();
                 super.run();
             }finally{
                 s_run.release();
             }
         }
-    }
 
-    @Override
-    public Thread newThread(Runnable r) {
-        return new ScheduledThread(r, runningThreads, ioThreads);
-        
+        private void EnterIO(URL url){
+            String host = url.getHost().toLowerCase();
+
+            if(!hosts.empty()){
+                //going to the same host is ok
+                if(!host.equals(hosts.peek())){
+                  logger.finer("ThreadExecutor: Nested EnterIO("+host+"); previous("+hosts.peek()+"); ignored");
+                }
+                hosts.push(host);
+                return;
+            }
+            String semaphoreGroup;
+            synchronized(hostgrp){
+                semaphoreGroup = hostgrp.get(host);
+                // first time not found, search for matching group
+                if (semaphoreGroup == null) {
+                    semaphoreGroup = ".*";
+                    for (String searchGroup : grouplimits.keySet()) {
+                        if (host.matches(searchGroup))
+                            if (searchGroup.length() > semaphoreGroup.length())
+                                semaphoreGroup = searchGroup;
+                    }
+                    logger.finer(String.format("IO download host: %s; rule: %s", host, semaphoreGroup));
+                    hostgrp.put(host, semaphoreGroup);
+                }
+            }
+
+            // there should be NO way to fail
+            //String dbgstr = "host="+host+"; thread="+getName();
+            //logger.finest("ThreadExecutor: Try EnterIO: "+dbgstr);
+            Semaphore s = grouplimits.get(semaphoreGroup);
+            s_iotarget = s;
+            s_run.release(); // exit running state; another thread might be released;
+            s_iotarget.acquireUninterruptibly(); // aquire URL target semaphore
+            hosts.push(host);
+            s_io.acquireUninterruptibly(); // enter io state
+            //logger.finest("ThreadExecutor: EnterIO done: "+dbgstr);
+            //ready to go...
+        }
+
+        private void LeaveIO(){
+            if (hosts.empty()) {
+                logger.fine(ThreadExecutor.getStackTrace(new Throwable("ThreadExecutor: Unbalanced LeaveIO call.")));
+                return;
+            }
+            String host = hosts.pop();
+            if (!hosts.empty()) {
+                if(!host.equals(hosts.peek())){
+                  logger.finer("Nested LeaveIO("+host+"); previous("+hosts.peek()+"); ignored");
+                }
+                return;
+            }
+    
+            //String dbgstr = "host="+host+"; thread="+getName();
+            s_iotarget.release();
+            s_io.release();
+            s_iotarget = null;
+            //logger.finest("ThreadExecutor: Try LeaveIO: "+dbgstr);
+            s_run.acquireUninterruptibly(); //back to running state
+            //logger.finest("ThreadExecutor: LeaveIO done: "+dbgstr);
+        }
     }
 
     static public void EnterIO(URL url){
-        String host = url.getHost().toLowerCase();
         if (!(Thread.currentThread() instanceof ScheduledThread)) {
-            logger.fine("Unmanaged thread call to EnterIO("+host+"); ignored.");
+            logger.fine(getStackTrace(new Throwable("ThreadExecutor: Unmanaged thread call to EnterIO; ignored.")));
             return;
         }
-        ScheduledThread st = (ScheduledThread)Thread.currentThread();
-        st.io_cnt ++;
-        if(st.io_cnt > 1){
-            logger.finer("Nested call to EnterIO("+host+"); previous("+st.iohost+"); ignored");
-            return;
-        }
-        String semaphoreGroup;
-        synchronized(hostgrp){
-            semaphoreGroup = hostgrp.get(host);
-            // first time not found, search for matching group
-            if (semaphoreGroup == null) {
-                semaphoreGroup = ".*";
-                for (String searchGroup : grouplimits.keySet()) {
-                    if (host.matches(searchGroup))
-                        if (searchGroup.length() > semaphoreGroup.length())
-                            semaphoreGroup = searchGroup;
-                }
-                logger.finer(String.format("IO download host: %s; rule: %s", host, semaphoreGroup));
-                hostgrp.put(host, semaphoreGroup);
-            }
-        }
-
-        // there should be NO way to fail
-        logger.finest("Enter IO mode; host="+host);
-        Semaphore s = grouplimits.get(semaphoreGroup);
-        st.s_iotarget = s;
-        st.iohost = host;
-        st.s_run.release(); // exit running state; another thread might be released;
-        st.s_iotarget.acquireUninterruptibly(); // aquire URL target semaphore
-        st.s_io.acquireUninterruptibly(); // enter io state
-        //ready to go...
+        ((ScheduledThread)Thread.currentThread()).EnterIO(url);
     }
 
     static public void EnterIO(String url){
@@ -164,7 +200,7 @@ public class ThreadExecutor<T> implements ThreadFactory{
             try {
                 u = new URL("http://"+url);
             } catch (MalformedURLException e1) {
-                logger.fine("Invalid call to EnterIO.");
+                logger.fine("ThreadExecutor: Invalid call to EnterIO.");
                 logger.fine(getStackTrace(e1));
                 return;
             }
@@ -174,35 +210,46 @@ public class ThreadExecutor<T> implements ThreadFactory{
 
     static public void LeaveIO(){
         if (!(Thread.currentThread() instanceof ScheduledThread)) {
-            logger.fine("Unscheduled thread call to LeaveIO; ignored.");
-            //logger.fine(getStackTrace(new Throwable("Unscheduled thread call to LeaveIO; ignored.")));
+            logger.fine(getStackTrace(new Throwable("ThreadExecutor: Unmanaged thread call to LeaveIO; ignored.")));
             return;
         }
-        ScheduledThread st = (ScheduledThread)Thread.currentThread();
-        if (st.io_cnt <= 0) {
-            logger.fine("Warning: unbalanced LeaveIO call; ignored.");
-            return;
-        }
-        st.io_cnt --;
-        if (st.io_cnt > 0) {
-            logger.finer("Nested LeaveIO call; ignored.");
-            return;
-        }
-        st.s_iotarget.release();
-        st.s_io.release();
-        st.s_iotarget = null;
-        st.s_run.acquireUninterruptibly(); //back to running state
-        logger.finest("Left IO mode; host="+st.iohost);
+        ((ScheduledThread)Thread.currentThread()).LeaveIO();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        return new ScheduledThread(r, runningThreads, ioThreads);        
     }
 
     public void restart(){
+        values.clear();
         runningThreads = new Semaphore(threads_run);
         ioThreads      = new Semaphore(threads_io);
-        pool = Executors.newFixedThreadPool(threads_run + threads_io, this);
+
+        //refined: use a fixed queue with some extra space; in relation with submit
+        //the size is just an approximation; it has no real connection to thread count
+        //make it reasonable sized to avoid waiting in submit
+        queue = new ArrayBlockingQueue<Runnable>(100);
+//        queue = new LinkedBlockingQueue<Runnable>();
+        //allow more threads, they are managed by semaphores 
+        pool = new ThreadPoolExecutor(threads_run, 2*threads_total,
+                        100, TimeUnit.MILLISECONDS,
+                        queue,
+                        this);
     }
 
-    public void submit(Callable<T> c){
+    public void submit(Callable<T> c) throws InterruptedException{
+        //never queue too many objects; wait for some space to limit resource allocations
+        //in case of fixed size queues, tasks could even be rejected
+        //therefore wait here a very short time
+        while (queue.remainingCapacity() <= 0)
+              Thread.sleep(5);
         values.add(pool.submit(c));
+    }
+
+    public void submit(Runnable r) throws InterruptedException{
+        T result = null;
+        submit(Executors.callable(r, result));
     }
 
     public static String getStackTrace(Throwable t)
@@ -217,7 +264,7 @@ public class ThreadExecutor<T> implements ThreadFactory{
 
     public ArrayList<T> waitForValues() throws Throwable{
         pool.shutdown();
-        ArrayList<T> v = new ArrayList<T>();
+        ArrayList<T> v = new ArrayList<T>(values.size());
         for(Future<T> f: values){
             try{
                 v.add(f.get());
@@ -236,5 +283,8 @@ public class ThreadExecutor<T> implements ThreadFactory{
 
     public void waitFor() throws Throwable{
         waitForValues();
+        int dif = threads_io - ioThreads.availablePermits(); 
+        if(dif != 0)
+            logger.severe("ThreadExecutor: unfinished downloading threads detected: " + dif);
     }
 }
