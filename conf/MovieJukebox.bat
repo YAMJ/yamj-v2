29fd09634c26991bf0007986b2d5c5ab66008b0b
Stuart Boston <omertron@gmail.com>
2010-04-21 15:00:01 +0000

    Increase maximum memory allocation in MovieJukebox.bat and MovieJukebox.sh files

--- conf/MovieJukebox.bat
+++ conf/MovieJukebox.bat
@@ -6,4 +6,4 @@ rem *** DO NOT CHANGE ANYTHING IN THIS SCRIPT
 rem *** UNLESS YOU KNOW WHAT YOU ARE DOING
 rem ***
 
-java -Xms256m -Xmx512m -classpath .;resources;lib/* com.moviejukebox.MovieJukebox %*
+java -Xms256m -Xmx1024m -classpath .;resources;lib/* com.moviejukebox.MovieJukebox %*
