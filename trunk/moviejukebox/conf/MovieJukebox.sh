3543b86faedd14f5e81c96c9032e1e186176adc8
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2010-04-21 15:00:01 +0000

    Increase maximum memory allocation in MovieJukebox.bat and MovieJukebox.sh files

--- trunk/moviejukebox/conf/MovieJukebox.sh
+++ trunk/moviejukebox/conf/MovieJukebox.sh
@@ -2,4 +2,4 @@
 # Intel Mac users : needs java 1.6, set fullpath to java 1.6 instead of default java
 # This path is usually : /System/Library/Frameworks/JavaVM.framework/Versions/1.6/Commands/java
 
-java -Xms256m -Xmx512m -classpath .:./resources:./lib/* com.moviejukebox.MovieJukebox "$@"
+java -Xms256m -Xmx1024m -classpath .:./resources:./lib/* com.moviejukebox.MovieJukebox "$@"
