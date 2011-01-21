faec1ef4c66bbed7c4b85191515d94d2a7aab0da
Stuart Boston <omertron@gmail.com>
2011-01-21 10:46:20 +0000

    Fixes issue 1796 Native RAR support NOTE: The property originally submitted has been changed too "mjb.scanner.archivescan.rar"

---
+++ src/com/moviejukebox/tools/ArchiveScanner.java
@@ -0,0 +1,12 @@
+package com.moviejukebox.tools;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+
+public interface ArchiveScanner {
+
+	// creates Files from mutableNames clearing them from the list.
+    public Collection<? extends File> getArchiveFiles(File parent, List<String> mutableNames);
+
+}
