1a775ceb49d668f27ad65227ee2a43acad422ade
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2011-02-24 11:15:42 +0000

    Fix minor bug with StringTokenizer and mjb.skin.copyDirs

--- trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/MovieJukebox.java
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/MovieJukebox.java
@@ -1162,7 +1162,7 @@ public class MovieJukebox {
                     logger.fine("Copying skin files to Jukebox directory...");
                 }
                 
-                StringTokenizer st = new StringTokenizer(PropertiesUtil.getProperty("mjb.skin.copyDirs", "html"));
+                StringTokenizer st = new StringTokenizer(PropertiesUtil.getProperty("mjb.skin.copyDirs", "html"), " ,;|");
                 
                 while (st.hasMoreTokens()) {
                     String skinDirName = st.nextToken();
