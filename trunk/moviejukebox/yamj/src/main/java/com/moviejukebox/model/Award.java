98fe14c8743d864736117a8ff51c5fdedcfd4c28
ilgizar@gmail.com <ilgizar@gmail.com@d118feeb-734d-0410-a58d-71a89b604941>
2011-04-29 22:06:36 +0000

    Issue 1901 Awards

---
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/model/Award.java
@@ -0,0 +1,65 @@
+/*
+ *      Copyright (c) 2004-2011 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      Web: http://code.google.com/p/moviejukebox/
+ *
+ *      This software is licensed under a Creative Commons License
+ *      See this page: http://code.google.com/p/moviejukebox/wiki/License
+ *
+ *      For any reuse or distribution, you must make clear to others the
+ *      license terms of this work.
+ */
+package com.moviejukebox.model;
+
+/*
+ * @author ilgizar
+ */
+public class Award {
+
+    private String name = Movie.UNKNOWN;
+    private String organization = Movie.UNKNOWN;
+    private int won = 0;
+    private int nominated = 0;
+    private int year = -1;
+
+    public String getOrg() {
+        return organization;
+    }
+
+    public void setOrg(String organization) {
+        this.organization = organization;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWon() {
+        return won;
+    }
+
+    public void setWon(int won) {
+        this.won = won;
+    }
+
+    public int getNominated() {
+        return nominated;
+    }
+
+    public void setNominated(int nominated) {
+        this.nominated = nominated;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
