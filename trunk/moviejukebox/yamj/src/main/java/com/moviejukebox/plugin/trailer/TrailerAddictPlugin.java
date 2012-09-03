fd9158a5ca2d89449025e013c4c9f3a747243b9e
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2012-09-03 12:00:05 +0000

    Added Trailer Addict Plugin

---
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/plugin/trailer/TrailerAddictPlugin.java
@@ -0,0 +1,122 @@
+/*
+ *      Copyright (c) 2004-2012 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      Web: http://code.google.com/p/moviejukebox/
+ *
+ *      This software is licensed under a Creative Commons License
+ *      See this page: http://code.google.com/p/moviejukebox/wiki/License
+ *
+ *      For any reuse or distribution, you must make clear to others the
+ *      license terms of this work.
+ */
+package com.moviejukebox.plugin.trailer;
+
+import com.moviejukebox.model.ExtraFile;
+import com.moviejukebox.model.Movie;
+import com.moviejukebox.model.MovieFile;
+import com.moviejukebox.plugin.ImdbPlugin;
+import com.moviejukebox.tools.FileTools;
+import com.moviejukebox.tools.PropertiesUtil;
+import com.moviejukebox.tools.StringTools;
+import com.moviejukebox.traileraddictapi.TrailerAddictApi;
+import com.moviejukebox.traileraddictapi.TrailerAddictException;
+import com.moviejukebox.traileraddictapi.model.Trailer;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+/**
+ * @author iuk
+ *
+ */
+public class TrailerAddictPlugin extends TrailersPlugin {
+
+    private static final Logger logger = Logger.getLogger(TrailerAddictPlugin.class);
+    private static final String logMessage = "TrailerAddict: ";
+    private int trailerMaxCount;
+
+    public TrailerAddictPlugin() {
+        super();
+        trailersPluginName = "TrailerAddict";
+        trailerMaxCount = PropertiesUtil.getIntProperty("traileraddict.max", "3");
+    }
+
+    @Override
+    public final boolean generate(Movie movie) {
+        movie.setTrailerLastScan(new Date().getTime()); // Set the last scan to now
+
+        String imdbId = movie.getId(ImdbPlugin.IMDB_PLUGIN_ID);
+        if (StringTools.isNotValidString(imdbId)) {
+            logger.debug(logMessage + "No IMDB Id found for " + movie.getBaseName() + ", trailers not downloaded");
+            return Boolean.FALSE;
+        }
+
+        List<Trailer> trailerList;
+        try {
+            trailerList = TrailerAddictApi.getFilmImdb(imdbId, trailerMaxCount);
+        } catch (TrailerAddictException ex) {
+            logger.warn(logMessage + "Failed to get trailer information: " + ex.getResponse());
+            return Boolean.FALSE;
+        }
+
+        if (trailerList.isEmpty()) {
+            logger.debug(logMessage + "No trailers found for " + movie.getBaseName());
+            return Boolean.FALSE;
+        }
+
+        for (Trailer trailer : trailerList) {
+            logger.debug(logMessage + "Found trailer at URL " + trailer.getLink());
+
+            MovieFile tmf = new MovieFile();
+            tmf.setTitle("TRAILER-" + trailer.getCombinedTitle());
+
+            String trailerUrl = getDownloadUrl(trailer);
+            if (StringTools.isValidString(trailerUrl)) {
+                if (getDownload()) {
+                    if (!downloadTrailer(movie, trailerUrl, FileTools.makeSafeFilename(trailer.getCombinedTitle()), tmf)) {
+                        return Boolean.FALSE;
+                    }
+                } else {
+                    tmf.setFilename(trailerUrl);
+                    movie.addExtraFile(new ExtraFile(tmf));
+                }
+            } else {
+                return Boolean.FALSE;
+            }
+        }
+
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public String getName() {
+        return trailersPluginName.toLowerCase();
+    }
+
+    /**
+     * Get the download URL for a trailer
+     *
+     * @param trailer
+     * @return
+     */
+    private String getDownloadUrl(Trailer trailer) {
+        String downloadPage;
+        try {
+            downloadPage = webBrowser.request(trailer.getTrailerDownloadUrl());
+        } catch (IOException ex) {
+            logger.warn(logMessage + "Failed to get webpage: " + ex.getMessage());
+            return Movie.UNKNOWN;
+        }
+
+        int startPos = downloadPage.indexOf("fileurl=");
+        if (startPos > -1) {
+            return downloadPage.substring(startPos + 8, downloadPage.indexOf("%", startPos));
+        } else {
+            logger.debug(logMessage + "Download URL not found for " + trailer.getCombinedTitle());
+        }
+
+        return Movie.UNKNOWN;
+    }
+}
---
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/tools/downloader/Downloader.java
@@ -0,0 +1,141 @@
+/*
+ *      Copyright (c) 2004-2012 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      Web: http://code.google.com/p/moviejukebox/
+ *
+ *      This software is licensed under a Creative Commons License
+ *      See this page: http://code.google.com/p/moviejukebox/wiki/License
+ *
+ *      For any reuse or distribution, you must make clear to others the
+ *      license terms of this work.
+ */
+package com.moviejukebox.tools.downloader;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.log4j.Logger;
+
+/**
+ * This is the downloader class.
+ *
+ * It will display the download progress of the file being processed.
+ *
+ * Taken from http://stackoverflow.com/a/11068356/443283
+ *
+ * @author stuart.boston
+ */
+public final class Downloader implements RBCWrapperDelegate {
+
+    private static final Logger LOGGER = Logger.getLogger(Downloader.class);
+    private static final String logMessage = "Downloader: ";
+    private static final String FORMAT_PERCENTAGE = "Download progress %,d Kb received, %.02f%%";
+    private static final String FORMAT_NOPER = "Download progress %,d Kb received";
+    private boolean showProgress = Boolean.TRUE;
+    private boolean downloadOk;
+    private long downloadTime;
+
+    public Downloader(String localPath, String remoteURL, boolean showProgress) {
+        this.showProgress = showProgress;
+        this.downloadOk = Boolean.FALSE;
+        // The time the download started
+        this.downloadTime = 0L;
+        long startTime = System.currentTimeMillis();
+
+        FileOutputStream fos = null;
+        ReadableByteChannel rbc = null;
+        URL url;
+
+        try {
+            url = new URL(remoteURL);
+            int contentLength = contentLength(url);
+
+            if (contentLength < 0) {
+                LOGGER.warn(logMessage + "WARNING: Remote URL is not a file! " + remoteURL);
+                return;
+            }
+
+            rbc = new RBCWrapper(Channels.newChannel(url.openStream()), contentLength, this);
+            fos = new FileOutputStream(localPath);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+            fos.flush();
+        } catch (MalformedURLException ex) {
+            LOGGER.debug(logMessage + "Failed to transform URL: " + ex.getMessage());
+        } catch (IOException ex) {
+            LOGGER.debug(logMessage + "Output error: " + ex.getMessage());
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException ex) {
+                LOGGER.trace(logMessage + "Failed to close output stream: " + ex.getMessage());
+            }
+
+            try {
+                if (rbc != null) {
+                    rbc.close();
+                }
+            } catch (IOException ex) {
+                LOGGER.trace(logMessage + "Failed to close ReadableByteChannel: " + ex.getMessage());
+            }
+        }
+
+        downloadOk = Boolean.TRUE;
+
+        // Calculate the download time.
+        downloadTime = System.currentTimeMillis() - startTime;
+    }
+
+    /**
+     * Format the download time
+     * @return
+     */
+    public String getDownloadTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return dateFormat.format(new Date(downloadTime));
+    }
+
+    @Override
+    public void rbcProgressCallback(RBCWrapper rbc, double progress) {
+        if (showProgress) {
+            if (progress > 0) {
+                System.out.println(String.format(FORMAT_PERCENTAGE, rbc.getKbReadSoFar(), progress));
+            } else {
+                System.out.println(String.format(FORMAT_NOPER, rbc.getKbReadSoFar()));
+            }
+        }
+    }
+
+    private int contentLength(URL url) {
+        HttpURLConnection connection;
+        int contentLength = -1;
+
+        try {
+            HttpURLConnection.setFollowRedirects(false);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+
+            contentLength = connection.getContentLength();
+        } catch (IOException ex) {
+            LOGGER.trace(logMessage + "Failed to get length from header: " + ex.getMessage());
+        }
+
+        return contentLength;
+    }
+
+    public boolean isDownloadOk() {
+        return downloadOk;
+    }
+}
