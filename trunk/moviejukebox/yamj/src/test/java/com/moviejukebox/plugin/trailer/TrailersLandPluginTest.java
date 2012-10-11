2809f8c58ac802102bd9489dffd521b2c47c4aa2
gianluca.vegetti@gmail.com <gianluca.vegetti@gmail.com@d118feeb-734d-0410-a58d-71a89b604941>
2012-10-11 15:12:19 +0000

    Test class for TrailersLandPlugin added

---
+++ trunk/moviejukebox/yamj/src/test/java/com/moviejukebox/plugin/trailer/TrailersLandPluginTest.java
@@ -0,0 +1,49 @@
+/*
+ *      Copyright (c) 2004-2012 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      Web: http://code.google.com/p/moviejukebox/
+ *
+ *      This software is licensed under a Creative Commons License
+ *      See this page: http://code.google.com/p/moviejukebox/wiki/License
+ *
+ *      For any reuse or distribution, you must make clear to others the
+ *      license terms of this work.
+ */
+package com.moviejukebox.plugin.trailer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Test;
+
+import com.moviejukebox.model.Movie;
+import com.moviejukebox.tools.PropertiesUtil;
+
+/**
+ *
+ * @author iuk
+ */
+
+public class TrailersLandPluginTest {
+
+    static {
+        PropertiesUtil.setProperty("trailers.download", "false");
+    }
+
+    private TrailersLandPlugin tlPlugin = new TrailersLandPlugin();
+
+    public TrailersLandPluginTest() {
+        BasicConfigurator.configure();
+    }
+
+    @Test
+    public void testGenerate() {
+
+        Movie movie = new Movie();
+        movie.setTitle("Paradiso Amaro");
+        movie.setOutline("The Descendants");
+        
+        assertTrue(tlPlugin.generate(movie));
+    }
+}
