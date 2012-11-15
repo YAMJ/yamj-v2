cca08694050c33dc757b54e75da4097e8e42928e
Stuart Boston <omertron@gmail.com>
2012-11-15 10:17:59 +0000

    Fixes issue 2493 Updated Sub-Baba poster search to use API, better search results

--- yamj/src/main/java/com/moviejukebox/plugin/poster/SubBabaPosterPlugin.java
+++ yamj/src/main/java/com/moviejukebox/plugin/poster/SubBabaPosterPlugin.java
@@ -15,17 +15,14 @@ package com.moviejukebox.plugin.poster;
 import com.moviejukebox.model.IImage;
 import com.moviejukebox.model.Image;
 import com.moviejukebox.model.Movie;
-import com.moviejukebox.scanner.artwork.PosterScanner;
 import com.moviejukebox.tools.PropertiesUtil;
+import com.moviejukebox.tools.StringTools;
 import com.moviejukebox.tools.WebBrowser;
 import com.omertron.subbabaapi.SubBabaApi;
 import com.omertron.subbabaapi.model.SearchType;
+import com.omertron.subbabaapi.model.SubBabaContent;
 import com.omertron.subbabaapi.model.SubBabaMovie;
-import java.awt.Dimension;
-import java.io.IOException;
-import java.net.URLEncoder;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 public class SubBabaPosterPlugin extends AbstractMoviePosterPlugin implements ITvShowPosterPlugin {
@@ -33,7 +30,6 @@ public class SubBabaPosterPlugin extends AbstractMoviePosterPlugin implements IT
     private static final Logger LOGGER = Logger.getLogger(SubBabaPosterPlugin.class);
     private static final String LOG_MESSAGE = "SubBabaPosterPlugin: ";
     private static final String API_KEY = PropertiesUtil.getProperty("API_KEY_SubBaba");
-    private WebBrowser webBrowser;
     private SubBabaApi subBaba;
 
     public SubBabaPosterPlugin() {
@@ -45,7 +41,12 @@ public class SubBabaPosterPlugin extends AbstractMoviePosterPlugin implements IT
         }
 
         subBaba = new SubBabaApi(API_KEY);
-        webBrowser = new WebBrowser();
+
+        // We need to set the proxy parameters if set.
+        subBaba.setProxy(WebBrowser.getMjbProxyHost(), WebBrowser.getMjbProxyPort(), WebBrowser.getMjbProxyUsername(), WebBrowser.getMjbProxyPassword());
+
+        // Set the timeout values
+        subBaba.setTimeout(WebBrowser.getMjbTimeoutConnect(), WebBrowser.getMjbTimeoutRead());
     }
 
     /**
@@ -57,145 +58,80 @@ public class SubBabaPosterPlugin extends AbstractMoviePosterPlugin implements IT
      */
     @Override
     public String getIdFromMovieInfo(String title, String year) {
-        List<SubBabaMovie> sbMovies = subBaba.searchByEnglishName(title, SearchType.POSTERS);
+        if (StringTools.isNotValidString(title)) {
+            return Movie.UNKNOWN;
+        }
+
+        LOGGER.debug(LOG_MESSAGE + "Searching for title '" + title + "' with year '" + year + "'");
+        // Use the ALL search type because we don't care what type there is
+        List<SubBabaMovie> sbMovies = subBaba.searchByEnglishName(title, SearchType.ALL);
 
         if (sbMovies != null && !sbMovies.isEmpty()) {
+            LOGGER.debug(LOG_MESSAGE + "Found " + sbMovies.size() + " movies");
             for (SubBabaMovie sbm : sbMovies) {
-                if (sbm != null && sbm.getId() > 0) {
-                    return Integer.toString(sbm.getId());
-                }
+                return sbm.getImdbId();
             }
         }
         return Movie.UNKNOWN;
     }
 
     /**
-     * retrieve the sub-baba.com poster url matching the specified movie name.
+     * Use the ID to find the poster
+     *
+     * @param id This should be the IMDB ID. Sub-Baba does not use it's own ID.
+     * @return
      */
-    public String getIdFromMovieInfo_OLD(String title, String year) {
-        String response = Movie.UNKNOWN;
-        try {
-//            String searchURL = "http://www.sub-baba.com/search?page=search&type=all&submit=%E7%F4%F9&search=" + URLEncoder.encode(title, "iso-8859-8");
-            StringBuilder searchURL = new StringBuilder("http://www.sub-baba.com/search/query/");
-            searchURL.append(URLEncoder.encode(title, "iso-8859-8"));
-            searchURL.append("/type/1/");
-
-            String xml = webBrowser.request(searchURL.toString());
-            String posterID = Movie.UNKNOWN;
-
-            int index = 0;
-            int endIndex = 0;
-            while (true) {
-                index = xml.indexOf("<div align=\"center\"><a href=\"/content/poster/", index);
-                if (index == -1) {
-                    break;
-                }
-
-                index += 45;
-
-                index = xml.indexOf("<a href=\"/content/poster/", index);
-                if (index == -1) {
-                    break;
-                }
-
-                index += 25;
-
-                endIndex = xml.indexOf("/\" ", index);
-                if (endIndex == -1) {
-                    break;
-                }
-
-                String scanPosterID = new String(xml.substring(index, endIndex));
-
-                index = endIndex + 3;
-
-                index = xml.indexOf("<span dir=\"ltr\">", index);
-                if (index == -1) {
-                    break;
-                }
-
-                index += 16;
-
-                endIndex = xml.indexOf("</span></a>", index);
-                if (endIndex == -1) {
-                    break;
-                }
-
-                String scanName = new String(xml.substring(index, endIndex)).trim();
-                index = endIndex + 11;
-
-                if (scanName.equalsIgnoreCase(title)) {
-                    posterID = scanPosterID;
-                    // We have a correct ID, so quit
-                    break;
-                }
-            }
-
-            if (!Movie.UNKNOWN.equals(posterID)) {
-                response = posterID;
-            }
-
-        } catch (Exception error) {
-            LOGGER.error(LOG_MESSAGE + "Failed retreiving SubBaba Id for movie : " + title);
-            LOGGER.error(LOG_MESSAGE + "Error : " + error.getMessage());
-        }
-        return response;
-    }
-
     @Override
     public IImage getPosterUrl(String id) {
-        if (StringUtils.isNumeric(id)) {
-            return Image.UNKNOWN;
-        } else {
+        LOGGER.debug(LOG_MESSAGE + "Searching for poster URL with ID: " + id);
+        if (StringTools.isNotValidString(id)) {
+            // Invalid ID
             return Image.UNKNOWN;
         }
-    }
 
-    public IImage getPosterUrl_OLD(String id) {
-        if (!Movie.UNKNOWN.equals(id)) {
-            Image posterImage = new Image();
-
-            // This is the page that contains the poster URL
-            StringBuilder posterUrl = new StringBuilder("http://www.sub-baba.com/download/");
-            posterUrl.append(id).append("/2");
-
-            // Load the poster page and extract the image filename
-            try {
-                String xml = webBrowser.request(posterUrl.toString());
-
-                int startIndex = xml.indexOf("<img src=\"");
-                if (startIndex == -1) {
-                    return posterImage;
-                }
-                startIndex += 10;
-
-                int endIndex = xml.indexOf("\" width=", startIndex);
-                if (endIndex == -1) {
-                    return posterImage;
-                }
-
-                posterUrl = new StringBuilder("http://www.sub-baba.com");
-                posterUrl.append(xml.substring(startIndex, endIndex));
-
-            } catch (IOException error) {
-                LOGGER.error(LOG_MESSAGE + "Failed retreiving SubBaba poster information (" + posterUrl + "): " + error.getMessage());
-            }
-
-            // Save the filename to the poster.
-            posterImage.setUrl(posterUrl.toString());
-
-            // checking poster dimension
-            Dimension imageDimension = PosterScanner.getUrlDimensions(posterUrl.toString());
-
-            // DVD Cover
-            if (imageDimension.getWidth() > imageDimension.getHeight()) {
-                LOGGER.debug(LOG_MESSAGE + "Detected DVD Cover, cropping image to poster size.");
+        IImage posterImage = getFirstContent(id, SearchType.POSTERS);
+        if (posterImage.equals(Image.UNKNOWN)) {
+            // No posters found, try DVD Covers
+            posterImage = getFirstContent(id, SearchType.DVD_COVERS);
+            if (!posterImage.equals(Image.UNKNOWN)) {
+                // Crop the image
+                LOGGER.debug(LOG_MESSAGE + "Found DVD Cover for ID " + id + ", cropping image to poster size.");
                 posterImage.setSubimage("0, 0, 47, 100");
             }
-
-            return posterImage;
         }
-        return Image.UNKNOWN;
+
+        if (!posterImage.equals(Image.UNKNOWN)) {
+            LOGGER.debug(LOG_MESSAGE + "Found poster URL: " + posterImage.getUrl());
+        }
+
+        return posterImage;
+    }
+
+    /**
+     * Find the first piece of content that matches the search type.
+     *
+     * @param imdbId
+     * @param searchType
+     * @return
+     */
+    private IImage getFirstContent(String imdbId, SearchType searchType) {
+        SubBabaMovie sbm = subBaba.searchByImdbId(imdbId, searchType);
+        if (sbm == null) {
+            // Nothing returned for the movie
+            LOGGER.debug(LOG_MESSAGE + "No information found for movie ID " + imdbId);
+            return Image.UNKNOWN;
+        }
+
+        List<SubBabaContent> content = sbm.getContent();
+
+        if (content == null || content.isEmpty()) {
+            // Nothing returned for the movie
+            LOGGER.debug(LOG_MESSAGE + "No " + searchType.toString() + " found for movie ID " + imdbId);
+            return Image.UNKNOWN;
+        }
+
+        // return the first poster's download URL
+        return new Image(content.get(0).getDownload());
     }
 
     @Override
