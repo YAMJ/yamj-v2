6f691e1ccc1d2f77e2912d8ea6b873c8a9b6f1c2
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2013-01-06 08:27:53 +0000

    Updated licence to GPLv3 Updated copyright notice

--- trunk/moviejukebox/mjbsqldb/src/main/java/com/moviejukebox/mjbsqldb/dto/VideoSiteDTO.java
+++ trunk/moviejukebox/mjbsqldb/src/main/java/com/moviejukebox/mjbsqldb/dto/VideoSiteDTO.java
@@ -1,16 +1,25 @@
 /*
- *      Copyright (c) 2004-2012 YAMJ Members
+ *      Copyright (c) 2004-2013 YAMJ Members
  *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      This file is part of the Yet Another Movie Jukebox (YAMJ).
+ *
+ *      The YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the YAMJ.  If not, see <http://www.gnu.org/licenses/>.
  *
  *      Web: http://code.google.com/p/moviejukebox/
  *
- *      This software is licensed under a Creative Commons License
- *      See this page: http://code.google.com/p/moviejukebox/wiki/License
- *
- *      For any reuse or distribution, you must make clear to others the
- *      license terms of this work.
  */
-
 package com.moviejukebox.mjbsqldb.dto;
 
 import java.io.Serializable;
