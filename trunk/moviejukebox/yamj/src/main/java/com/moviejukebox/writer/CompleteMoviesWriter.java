dfad4745ac27ecb6febe6acfa99c5db87c31989d
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2013-01-07 14:34:00 +0000

    Updated CompleteMoviesWriter to close file after use.

--- trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/writer/CompleteMoviesWriter.java
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/writer/CompleteMoviesWriter.java
@@ -26,10 +26,13 @@ import com.moviejukebox.MovieJukebox;
 import com.moviejukebox.model.Jukebox;
 import com.moviejukebox.model.Library;
 import com.moviejukebox.tools.FileTools;
+import com.moviejukebox.tools.StringTools;
 import com.moviejukebox.tools.SystemTools;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Level;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.transform.Result;
@@ -55,7 +58,14 @@ public class CompleteMoviesWriter {
         throw new UnsupportedOperationException("Class cannot be initialised");
     }
 
-    public static boolean writeCompleteMovies(Library library, Jukebox jukebox) {
+    /**
+     * Write the CompleteMovies file to the jukebox
+     *
+     * @param library
+     * @param jukebox
+     * @return
+     */
+    public static boolean generate(Library library, Jukebox jukebox) {
         JAXBContext context;
 
         try {
@@ -70,31 +80,41 @@ public class CompleteMoviesWriter {
         jukeboxXml.movies = library.values();
 
         File totalMoviesXmlFile = new File(jukebox.getJukeboxTempLocationDetails(), completeMoviesXmlFileName);
+        File rootTotalMoviesFile = FileTools.fileCache.getFile(StringTools.appendToPath(jukebox.getJukeboxRootLocationDetails(), completeMoviesXmlFileName));
 
-        if (library.isDirty() || !totalMoviesXmlFile.exists()) {
+        if (library.isDirty() || !rootTotalMoviesFile.exists()) {
+            OutputStream marStream = null;
             try {
-                OutputStream marStream = FileTools.createFileOutputStream(totalMoviesXmlFile);
+                marStream = FileTools.createFileOutputStream(totalMoviesXmlFile);
                 context.createMarshaller().marshal(jukeboxXml, marStream);
-                // This needs to be done here so that the transformer can read it.
-                marStream.flush();
-                marStream.close();
+            } catch (JAXBException ex) {
+                logger.warn(LOG_MESSAGE + "RSS is not generated (JAXB error): " + ex.getMessage());
+                logger.warn(SystemTools.getStackTrace(ex));
+                return Boolean.FALSE;
+            } catch (FileNotFoundException ex) {
+                logger.warn(LOG_MESSAGE + "RSS is not generated (Jukebox error): " + ex.getMessage());
+                logger.warn(SystemTools.getStackTrace(ex));
+                return Boolean.FALSE;
+            } finally {
+                if (marStream != null) {
+                    try {
+                        marStream.flush();
+                        marStream.close();
+                    } catch (IOException ex) {
+                        logger.trace(LOG_MESSAGE + "Failed to close marshal file: " + ex.getMessage());
+                    }
+                }
+            }
 
+            try {
                 Transformer transformer = MovieJukeboxHTMLWriter.getTransformer(new File(rssXslFileName), jukebox.getJukeboxRootLocationDetails());
 
                 Result xmlResult = new StreamResult(new File(jukebox.getJukeboxTempLocationDetails(), rssXmlFileName));
                 transformer.transform(new StreamSource(totalMoviesXmlFile), xmlResult);
 
                 logger.debug(LOG_MESSAGE + "RSS has been generated.");
-            } catch (JAXBException ex) {
-                logger.warn(LOG_MESSAGE + "RSS is not generated (JAXB error): " + ex.getMessage());
-                logger.warn(SystemTools.getStackTrace(ex));
-                return Boolean.FALSE;
             } catch (TransformerException ex) {
                 logger.warn(LOG_MESSAGE + "RSS is not generated (Transformer error): " + ex.getMessage());
-                logger.warn(SystemTools.getStackTrace(ex));
-                return Boolean.FALSE;
-            } catch (IOException ex) {
-                logger.warn(LOG_MESSAGE + "RSS is not generated (Jukebox error): " + ex.getMessage());
                 logger.warn(SystemTools.getStackTrace(ex));
                 return Boolean.FALSE;
             }
