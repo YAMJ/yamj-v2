24fcbed437972eb3773e573850d9b164526ef575
Stuart.Boston <Stuart.Boston@d118feeb-734d-0410-a58d-71a89b604941>
2013-02-18 10:01:19 +0000

    Changed WatchedScanner to use enums for location and extension settings

---
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/model/enumerations/WatchedWithExtension.java
@@ -0,0 +1,43 @@
+package com.moviejukebox.model.enumerations;
+
+import java.util.EnumSet;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Check the watched file with the extension of the video, without or both
+ *
+ * @author Stuart
+ */
+public enum WatchedWithExtension {
+
+    EXTENSION("true"),
+    NOEXTENSION("false"),
+    BOTH("both");
+    private String type;
+
+    private WatchedWithExtension(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * Convert a string into an Enum type
+     *
+     * @param artworkType
+     * @return
+     */
+    public static WatchedWithExtension fromString(String extensionString) {
+        if (StringUtils.isNotBlank(extensionString)) {
+            for (final WatchedWithExtension extension : EnumSet.allOf(WatchedWithExtension.class)) {
+                if (extensionString.equalsIgnoreCase(extension.type)) {
+                    return extension;
+                }
+            }
+        }
+        // We've not found the type, so return both
+        return BOTH;
+    }
+}
---
+++ trunk/moviejukebox/yamj/src/main/java/com/moviejukebox/model/enumerations/WatchedWithLocation.java
@@ -0,0 +1,67 @@
+/*
+ *      Copyright (c) 2004-2013 YAMJ Members
+ *      http://code.google.com/p/moviejukebox/people/list
+ *
+ *      This file is part of the Yet Another Movie Jukebox (YAMJ).
+ *
+ *      The YAMJ is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      any later version.
+ *
+ *      YAMJ is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with the YAMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *      Web: http://code.google.com/p/moviejukebox/
+ *
+ */
+package com.moviejukebox.model.enumerations;
+
+import java.util.EnumSet;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Location of the watched files
+ *
+ * @author Stuart
+ */
+public enum WatchedWithLocation {
+
+    /*
+     * Watched file is stored with the video
+     */
+    withVideo,
+    /*
+     * Watched file is stored with the jukebox
+     */
+    withJukebox,
+    /*
+     * Watched files are stored elsewhere
+     */
+    custom;
+
+    /**
+     * Convert a string into an Enum type
+     *
+     * @param artworkType
+     * @return
+     * @throws IllegalArgumentException If type is not recognised
+     *
+     */
+    public static WatchedWithLocation fromString(String location) {
+        if (StringUtils.isNotBlank(location)) {
+            for (final WatchedWithLocation withLocation : EnumSet.allOf(WatchedWithLocation.class)) {
+                if (location.equalsIgnoreCase(withLocation.toString())) {
+                    return withLocation;
+                }
+            }
+        }
+        // We've not found the type, so reutrn custom
+        return custom;
+    }
+}
