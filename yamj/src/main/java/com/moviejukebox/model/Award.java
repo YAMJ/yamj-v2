551fcf54ca0df9b3599d09453a931d719e6097b4
Stuart Boston <omertron@gmail.com>
2015-03-10 11:48:40 +0000

    Fix null check in Award.java

--- yamj/src/main/java/com/moviejukebox/model/Award.java
+++ yamj/src/main/java/com/moviejukebox/model/Award.java
@@ -48,7 +48,7 @@ public class Award {
     }
 
     public int getWon() {
-        return (wons != null && wons.isEmpty()) ? won : wons.size();
+        return (wons == null || wons.isEmpty()) ? won : wons.size();
     }
 
     public void setWon(int won) {
@@ -56,7 +56,7 @@ public class Award {
     }
 
     public int getNominated() {
-        return (nominations != null && nominations.isEmpty()) ? nominated : nominations.size();
+        return (nominations == null || nominations.isEmpty()) ? nominated : nominations.size();
     }
 
     public void setNominated(int nominated) {
